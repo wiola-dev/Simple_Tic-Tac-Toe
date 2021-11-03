b95fe11249df0f270e177a4e14644cd2fd7bdec2
WiolaPL <violeet.pl@gmail.com>
2021-11-03 20:25:47 +0100

    Count how many times a number occurs

--- Topics/Iterating over arrays/Count how many times a number occurs/src/Main.java
+++ Topics/Iterating over arrays/Count how many times a number occurs/src/Main.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
         int sizeOfArray = scanner.nextInt();
         int[] array = new int[sizeOfArray];
-        int sum = 0;
 
         for (int i = 0; i < sizeOfArray; i++) {
             array[i] = scanner.nextInt();
-            sum += array[i];
         }
-        System.out.println(sum);
+
+        int numberN = scanner.nextInt();
+        int counterN = 0;
+        for (int number : array) {
+            if (number == numberN) {
+                counterN++;
+            }
+        }
+        System.out.println(counterN);
     }
 }
