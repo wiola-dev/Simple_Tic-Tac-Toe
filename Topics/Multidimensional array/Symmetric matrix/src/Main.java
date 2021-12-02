0112a9db6e2b17b52aa1dea8b3916714a9b8ce58
WiolaPL <violeet.pl@gmail.com>
2021-12-02 01:39:27 +0100

    Symmetric matrix

--- Topics/Multidimensional array/Symmetric matrix/src/Main.java
+++ Topics/Multidimensional array/Symmetric matrix/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = scanner.nextInt();
+
+        if (arraySize == 1) {
+            System.out.println("YES");
+        } else {
+            int[][] matrix = new int[arraySize][arraySize];
+            for (int row = 0; row < arraySize; row++) {
+                for (int col = 0; col < arraySize; col++) {
+                    matrix[row][col] = scanner.nextInt();
+                }
+            }
+            boolean isSymmetric = true;
+            for (int row = 0; row < arraySize; row++) {
+                if (!isSymmetric) {
+                    break;
+                }
+                for (int col = row + 1; col < arraySize; col++) {
+                    if (matrix[row][col] != matrix[col][row]) {
+                        isSymmetric = false;
+                        break;
+                    }
+                }
+            }
+            System.out.println(isSymmetric ? "YES" : "NO");
+        }
     }
 }
